944ced3838c352a21ea339cdbd2245d93b25360b
foxcoonn <foxcoonn1@gmail.com>
2021-09-09 16:47:55 +0300

    файлы добавлены под гит

---
+++ resources/views/info.blade.php
@@ -0,0 +1,2 @@
+<b>🔵 Info{{ isset($h1) ? " {$h1}" : '' }}</b>{{ isset($h2) ? "\n{$h2}" : '' }}{!! !empty($text) ? "\n{$text}" : '' !!}
+<b>{{ isset($source) ? $source : config('app.url') }}</b>
---
+++ src/Messages/Error.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Indeximstudio\LaravelTelegramMessages\Messages;
+
+use Illuminate\Support\Arr;
+use Illuminate\View\View;
+use Throwable;
+
+class Error extends Message
+{
+    /**
+     * @param $text
+     * @param array $params
+     * @return static
+     * @throws Throwable
+     */
+    public static function make($text, $params = []): Error
+    {
+        return new static($text, $params);
+    }
+
+    /**
+     * @param View|array|string $text
+     * @param array $params
+     * @throws Throwable
+     */
+    public function __construct($text, $params = [])
+    {
+        parent::__construct(
+            config('laravel-telegram-messages.chatId.error'),
+            $this->prepareText($text),
+            Arr::get($params, 'parse_mode'),
+            Arr::get($params, 'disable_web_page_preview'),
+            Arr::get($params, 'disable_notification'),
+            Arr::get($params, 'reply_to_message_id'),
+            Arr::get($params, 'reply_markup')
+        );
+
+        \Log::error($this->text);
+    }
+
+    protected function defaultViewName(): string
+    {
+        return 'telegram-messages:error';
+    }
+}
